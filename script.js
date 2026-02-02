2363c5f177226dae9bc049fd22422bcc89b917af
Beca <234265987+becsnr@users.noreply.github.com>
2026-02-02 10:41:56 -0300

    frase "motivacional"

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 const imagem = document.getElementById('imagem');
 const botoes = document.querySelectorAll('#botoes button');
+const frase = document.getElementById('frase');
 
 botoes.forEach(botao => {
     botao.addEventListener("click", () => {
         const emocao = botao.dataset.emocao;
         const backCor = botao.dataset.cor;
         const imgPng = botao.dataset.img;
+        const fraseEmocao = botao.dataset.frase
 
         document.body.style.backgroundColor = backCor;
 
         imagem.src = imgPng;
         imagem.style.filter = 'drop-shadow(0 10px 20px #0000005e)';
+
+        frase.innerHTML = fraseEmocao;
 
         console.log(emocao);
         console.log(backCor);
